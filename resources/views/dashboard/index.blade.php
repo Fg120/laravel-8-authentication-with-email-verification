25ab816c296c76e2f203f34c95e39d9c2d134a93
Fg120 <anwaria1404@gmail.com>
2023-12-14 02:37:11 +0700

    Initial commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app-master')
+
+@section('content')
+    <div class="bg-light p-5 rounded">
+        <h1>Dashboard</h1>
+        <p class="lead">Only authenticated users can access this section.</p>
+        <a class="btn btn-lg btn-primary" href="https://codeanddeploy.com" role="button">View more tutorials here &raquo;</a>
+        <h1>PAGE 111</h1>
+        <h1>PAGE 111</h1>
+        <h1>PAGE 111</h1>
+        <h1>PAGE 111</h1>
+        <h1>PAGE 111</h1>
+        <h1>PAGE 111</h1>
+        <h1>PAGE 111</h1>
+    </div>
+@endsection
